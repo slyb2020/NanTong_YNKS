780100c7941ff0ff7fd3cbbd3552e35395f495e7
slyb2020 <slyb@tju.edu.cn>
2022-03-12 13:07:09 +0800

    2022年3月12日13:06在ASUS上提交,完成密码登录及注销登录功能

---
+++ DBOperation.py
@@ -0,0 +1,43 @@
+from ID_DEFINE import *
+import pymysql as MySQLdb
+import time
+import datetime
+
+
+def GetAllPasswords(log, whichDB):
+    try:
+        # db = MySQLdb.connect(host="127.0.0.1", user="root", passwd='', db="智能生产管理系统_调试",charset='utf8')
+        db = MySQLdb.connect(host="%s" % dbHostName[whichDB], user='%s' % dbUserName[whichDB],
+                             passwd='%s' % dbPassword[whichDB], db='%s' % dbName[whichDB], charset='utf8')
+    except:
+        wx.MessageBox("无法连接智能生产管理系统数据库!", "错误信息")
+        if log:
+            log.WriteText("无法连接智能生产管理系统数据库", colour=wx.RED)
+        return -1, []
+    cursor = db.cursor()
+    sql = """SELECT `密码` from `info_staff` """
+    cursor.execute(sql)
+    temp = cursor.fetchall()  # 获得压条信息
+    data = []
+    for psw in temp:
+        data.append(psw[0])
+    db.close()
+    return 0, data
+
+
+def GetOperatorNameWithPassword(log, whichDB, psw):
+    try:
+        # db = MySQLdb.connect(host="127.0.0.1", user="root", passwd='', db="智能生产管理系统_调试",charset='utf8')
+        db = MySQLdb.connect(host="%s" % dbHostName[whichDB], user='%s' % dbUserName[whichDB],
+                             passwd='%s' % dbPassword[whichDB], db='%s' % dbName[whichDB], charset='utf8')
+    except:
+        wx.MessageBox("无法连接智能生产管理系统数据库!", "错误信息")
+        if log:
+            log.WriteText("无法连接智能生产管理系统数据库", colour=wx.RED)
+        return -1, []
+    cursor = db.cursor()
+    sql = """SELECT `姓名` from `info_staff` WHERE `密码`='%s'"""%(psw)
+    cursor.execute(sql)
+    temp = cursor.fetchone()  # 获得压条信息
+    db.close()
+    return 0, temp[0]
---
+++ PasswordDialog.py
@@ -0,0 +1,70 @@
+import wx
+import os
+from DBOperation import GetAllPasswords
+
+dirName = os.path.dirname(os.path.abspath(__file__))
+bitmapDir = os.path.join(dirName, 'bitmaps')
+
+
+class PasswordDialog(wx.Dialog):
+    def __init__(self, parent, size=wx.DefaultSize, pos=wx.DefaultPosition,
+                 style=wx.DEFAULT_DIALOG_STYLE):
+        wx.Dialog.__init__(self)
+        self.parent = parent
+        # self.log.WriteText("操作员：'%s' 开始执行库存参数设置操作。。。\r\n"%(self.parent.operator_name))
+        self.SetExtraStyle(wx.DIALOG_EX_METAL)
+        self.Create(parent, -1, "惭怍员登录对话框", pos, size, style)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        panel = wx.Panel(self, -1, size=(300, 60))
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        vbox.Add((-1, 20))
+        hbox = wx.BoxSizer()
+        hbox.Add(10, -1)
+        hbox.Add(wx.StaticText(panel, label='请输入您的登录密码：'), 0, wx.TOP, 5)
+        self.pswTXT = wx.TextCtrl(panel, size=(100, 25), style=wx.TE_PASSWORD)
+        hbox.Add(self.pswTXT, 1, wx.LEFT | wx.RIGHT, 10)
+        vbox.Add(hbox, 0, wx.EXPAND)
+        panel.SetSizer(vbox)
+        # panel.SetBackgroundColour(wx.Colour(234,219,212))
+        sizer.Add(panel, 0, wx.EXPAND)
+        line = wx.StaticLine(self, -1, size=(30, -1), style=wx.LI_HORIZONTAL)
+        sizer.Add(line, 0, wx.GROW | wx.RIGHT | wx.TOP, 5)
+        btnsizer = wx.BoxSizer()
+        bitmap1 = wx.Bitmap("bitmaps/ok4.png", wx.BITMAP_TYPE_PNG)
+        bitmap2 = wx.Bitmap("bitmaps/cancel1.png", wx.BITMAP_TYPE_PNG)
+        btn_ok = wx.Button(self, wx.ID_OK, "确定", size=(120, 35))
+        btn_ok.SetBitmap(bitmap1, wx.LEFT)
+        btn_cancel = wx.Button(self, wx.ID_CANCEL, "取消", size=(120, 35))
+        btn_cancel.SetBitmap(bitmap2, wx.LEFT)
+        btnsizer.Add(btn_ok, 0)
+        btnsizer.Add((40, -1), 0)
+        btnsizer.Add(btn_cancel, 0)
+        sizer.Add(btnsizer, 0, wx.ALIGN_CENTER | wx.ALL, 10)
+        self.SetSizer(sizer)
+        sizer.Fit(self)
+        btn_ok.Bind(wx.EVT_BUTTON, self.OnOk)
+        btn_cancel.Bind(wx.EVT_BUTTON, self.OnCancel)
+        self.tryTimes=4
+        _, self.parent.pswList = GetAllPasswords(None,1)
+
+    def OnOk(self, event):
+        psw = self.pswTXT.GetValue()
+        if psw not in self.parent.pswList:
+            self.tryTimes -= 1
+            if self.tryTimes>0:
+                self.pswTXT.SetValue('')
+                self.pswTXT.SetFocus()
+                dlg = wx.MessageDialog(self, '您输入的密码错误，您还有%d次机会重新输入！'%self.tryTimes,
+                                       '信息提示',
+                                       wx.OK | wx.ICON_INFORMATION
+                                       # wx.YES_NO | wx.NO_DEFAULT | wx.CANCEL | wx.ICON_INFORMATION
+                                       )
+                dlg.ShowModal()
+                dlg.Destroy()
+                return
+        # self.log.WriteText("操作员：'%s' 完成库存参数设置操作\r\n"%(self.parent.operator_name))
+        event.Skip()
+
+    def OnCancel(self, event):
+        # self.log.WriteText("操作员：'%s' 取消库存参数设置操作\r\n"%(self.parent.operator_name))
+        event.Skip()
